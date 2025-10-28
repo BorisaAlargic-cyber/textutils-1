2630426c02574a060980575dde21220c557df496
BorisaAlargic-cyber <alargicborisa@gmail.com>
2025-10-28 20:43:42 +0100

    Making new things

--- src/textutils/core.py
+++
@@ -1,7 +0,0 @@
-import re
-
-def camel_to_snake(text): 
-    # Insert underscore between a lowercase/number and uppercase letter
-    text = re.sub(r'(?<=[a-z0-9])([A-Z])', r'_\1', text)
-    # Convert the whole string to lowercase
-    return text.lower()
